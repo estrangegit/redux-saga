3ebb1c0378b23458da04f13ff8d6aef2136315b3
Etienne Estrangin <etienne.estrangin@cognizant.com>
2018-11-27 17:53:04 +0100

    Simple use-case for redux-saga

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-import "babel-polyfill"
+import "babel-polyfill";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-import Counter from './Counter'
-import reducer from './reducers'
+import Counter from './Counter';
+import reducer from './reducers';
 
-const store = createStore(reducer)
+import rootSaga from './Sagas';
+
+const sagaMiddleWare = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleWare));
+
+sagaMiddleWare.run(rootSaga);
 
 const action = type => store.dispatch({type})
 
@@ -16,7 +22,8 @@ function render() {
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')} 
+      onIncrementAsync={() => action('INCREMENT_ASYNC')}/>,
     document.getElementById('root')
   )
 }
